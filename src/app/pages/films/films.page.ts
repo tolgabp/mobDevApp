4a93616ece1a049dc83dacd504c1a89a5a473090
Tolga Baris Pinar <68820772+tolgabp@users.noreply.github.com>
2023-05-12 01:01:07 +0100

    event bug fixed and working on infinite scroll

--- src/app/pages/films/films.page.ts
+++ src/app/pages/films/films.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, NavController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, LoadingController, NavController } from '@ionic/angular';
 import { FilmDetailsPage } from '../film-details/film-details.page';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
@@ -11,20 +12,21 @@ import { FilmDetailsPage } from '../film-details/film-details.page';
 })
 export class FilmsPage implements OnInit {
 
-  movies : any[] = [];
+  movies: any[] = [];
   currentPage = 1;
+  imageBaseUrl = environment.images;
 
-  constructor(private navController: NavController, 
-    private router: Router, 
-    private filmDetailsPage: FilmDetailsPage, 
+  constructor(private navController: NavController,
+    private router: Router,
+    private filmDetailsPage: FilmDetailsPage,
     private loadingCtrl: LoadingController
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.loadMovies();
   }
 
-  async loadMovies() {
+  async loadMovies(event?: InfiniteScrollCustomEvent) {
     const loading = await this.loadingCtrl.create({
       message: 'Loading...',
       spinner: 'bubbles',
@@ -33,9 +35,20 @@ export class FilmsPage implements OnInit {
 
     this.filmDetailsPage.getTopRatedFilms(this.currentPage).subscribe((res) => {
       loading.dismiss();
-      this.movies = [...this.movies, ...res.results];
+      //this.movies = [...this.movies, ...res.results];
+      this.movies.push(...res.results);
       console.log(res);
+
+      event?.target.complete();
+      if(event){
+        event.target.disabled = res.total_pages === this.currentPage;
+      }
     }); // to get the data from observable we need to subsribe to it
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
   }
 
 
